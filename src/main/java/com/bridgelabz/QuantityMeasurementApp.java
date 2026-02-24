b2479e32f74f10f7dc9e8733a7d6570c23127b25
S KARTHIK <skarthik.link@gmail.com>
2026-02-24 16:20:52 +0530

    feat: implement UC2 Inch equality

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -20,9 +20,31 @@ public class QuantityMeasurementApp {
         }
     }
 
+    public static class Inch {
+        private final double value;
+
+        public Inch(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Inch inch = (Inch) obj;
+            return Double.compare(inch.value, value) == 0;
+        }
+    }
+
     public static void main(String[] args) {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
-        System.out.println("Equal: " + f1.equals(f2));
+        Inch i1 = new Inch(12.0);
+        Inch i2 = new Inch(12.0);
+
+        System.out.println("Feet Equal: " + f1.equals(f2));
+        System.out.println("Inch Equal: " + i1.equals(i2));
     }
 }
--- src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
 
+    // UC1 - Feet
+
     @Test
     void givenSameFeetValue_WhenCompared_ShouldReturnTrue() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
@@ -20,20 +22,42 @@ public class QuantityMeasurementAppTest {
     }
 
     @Test
-    void givenNull_WhenCompared_ShouldReturnFalse() {
+    void givenNullFeet_WhenCompared_ShouldReturnFalse() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
         assertFalse(f1.equals(null));
     }
 
     @Test
-    void givenDifferentType_WhenCompared_ShouldReturnFalse() {
+    void givenSameReferenceFeet_WhenCompared_ShouldReturnTrue() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
-        assertFalse(f1.equals("1.0"));
+        assertTrue(f1.equals(f1));
+    }
+
+    // UC2 - Inch
+
+    @Test
+    void givenSameInchValue_WhenCompared_ShouldReturnTrue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(12.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(12.0);
+        assertTrue(i1.equals(i2));
     }
 
     @Test
-    void givenSameReference_WhenCompared_ShouldReturnTrue() {
-        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
-        assertTrue(f1.equals(f1));
+    void givenDifferentInchValue_WhenCompared_ShouldReturnFalse() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(12.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(24.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void givenNullInch_WhenCompared_ShouldReturnFalse() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(12.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void givenSameReferenceInch_WhenCompared_ShouldReturnTrue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(12.0);
+        assertTrue(i1.equals(i1));
     }
 }
